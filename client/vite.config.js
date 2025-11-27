063d96fb5b163f96860e37401a44e1bc83a5cefb
tncreditsolutions <tncreditsolutions@gmail.com>
2025-11-27 16:31:16 -0500

    Add files via upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server:{ proxy:{ '/api':{ target:'http://localhost:3000', changeOrigin:true } } }
+  server: {
+    port: 5173
+  }
 });
